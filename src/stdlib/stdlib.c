997854fc5059c9e43028c8788a2676de8d027bc2
Vladimir Meshkov <ubob74@gmail.com>
2020-12-26 10:24:07 +0300

    Add lost stdlib.c file

---
+++ src/stdlib/stdlib.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+/* TODO */
+
+size_t memcpy(void *dst, const void *src, size_t size)
+{
+	int i;
+
+	if (!dst || !src)
+		return 0;
+
+	for (i = 0; i < size; i++)
+		*((uint8_t *)dst + i) = *((uint8_t *)src + i);
+
+	return size;
+}
+
+int strlen(const char *src)
+{
+	int i = 0;
+
+	if (src)
+		for (; src[i] != 0x00; i++);
+
+	return i;
+}
+
+int strcmp(const char *src, const char *dst)
+{
+	int i, len;
+
+	if (!src || !dst)
+		return -1;
+
+	len = strlen(src);
+
+	for (i = 0; i < len; i++) {
+		if (src[i] != dst[i])
+			return -1;
+	}
+
+	if (dst[i] != 0x00)
+		return -1;
+
+	return 0;
+}
